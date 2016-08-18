0ad64ce9cc0d70bdd8799631c777beac7c94dc2c
zcq <980242860@qq.com>
2016-08-18 22:37:29 +0800

    end

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -62,7 +62,7 @@ function getDefaultModules() {
 
 module.exports = {
   srcPath: srcPath,
-  publicPath: 'assets/',
+  publicPath: '/assets/',
   port: dfltPort,
   getDefaultModules: getDefaultModules
 };
